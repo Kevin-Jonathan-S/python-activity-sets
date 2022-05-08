31a9211ed877316c040a9f5f62f5ef9dd31b03fa
Kevin-Jonathan <94686007+Kevin-Jonathan-S@users.noreply.github.com>
2022-05-08 11:07:06 +0000

    Programmed Activity Set 1, Program 4

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,11 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+hrs = float(input("Enter hours: "))
+rate = float(input("Enter rate per hour: "))
+if hrs > 40:
+  bonusrate = rate * 1.5
+  bonuspay = (hrs - 40) * bonusrate
+  pay = ((hrs - (hrs - 40)) * rate) + bonuspay
+else:
+  pay = hrs * rate
+print("Your pay is: $", pay)
